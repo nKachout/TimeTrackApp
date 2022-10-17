9fecf78eba887a137b9233eaf3bfca394c139682
Najim Kachout <najimkachout4904@gmail.com>
2022-10-17 18:55:18 +0200

    correction erreur verification compte existant

--- model/user.js
+++ model/user.js
@@ -1,43 +1,36 @@
+// Importing modules
+const mongoose = require("mongoose");
+var crypto = require("crypto");
 
-// Importing modules 
-const mongoose = require('mongoose'); 
-var crypto = require('crypto'); 
-  
-// Creating user schema 
-const UserSchema = mongoose.Schema({ 
-    username : { 
-        type : String, 
-        required : true
-    }, 
-    email : { 
-        type : String, 
-        required : true
-    }, 
-    passwordHash : String, 
-    salt : String 
-}); 
+// Creating user schema
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  passwordHash: String,
+  salt: String,
+});
 
-// Method to set salt and hash the password for a user 
-UserSchema.methods.setPassword = function(password) { 
-     
-    this.salt = crypto.randomBytes(16).toString('hex'); 
-    this.passwordHash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`); 
+// Method to set salt and hash the password for a user
+UserSchema.methods.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.passwordHash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, `sha512`)
+    .toString(`hex`);
+};
 
-}; 
-  
-// Method to check the entered password is correct or not 
-UserSchema.methods.validPassword = function(password) { 
+// Method to check the entered password is correct or not
+UserSchema.methods.validPassword = function (password) {
+  var hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, `sha512`)
+    .toString(`hex`);
+  return this.passwordHash === hash;
+};
 
-    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`); 
-    return this.passwordHash === hash; 
-
-}; 
-var UserSchemaDb = mongoose.model('UserSchema', UserSchema );
-UserSchema.methods.isPresent = function(_username) { 
-
-    return UserSchemaDb.findOne({username: _username});
-
-}; 
-  
-// Exporting module to allow it to be imported in other files 
-const User = module.exports = mongoose.model('User', UserSchema);
+// Exporting module to allow it to be imported in other files
+const User = (module.exports = mongoose.model("User", UserSchema));
--- route/user.js
+++ route/user.js
@@ -1,70 +1,66 @@
-// Importing modules 
-const express = require('express'); 
-const { db } = require('../model/user');
-const router = express.Router(); 
-// Importing User Schema 
-const User = require('../model/user'); 
+// Importing modules
+const express = require("express");
+const { db } = require("../model/user");
+const router = express.Router();
+// Importing User Schema
+const User = require("../model/user");
 
-// User login api 
-router.post('/login', (req, res) => { 
-    console.log("user login"); 
-    // Find user with requested email 
-    User.findOne({ username : req.body.username }, function(err, user) { 
-        if (user === null) { 
-            return res.status(400).render("loginPage.ejs",{ 
-                message : "Ce compte n'existe pas"
-            });
-        } 
-        else { 
-            if (user.validPassword(req.body.password)) { 
-                return res.status(201).render("index.ejs",{ 
-                    ma_variable : req.body.username
-                });
-            } 
-            else { 
-                return res.status(400).render("loginPage.ejs",{ 
-                    message : "Nom d'utilisateur, ou mot de passe incorrect"
-                });
-            } 
-        } 
-    }); 
-}); 
-
-// User signup api 
-router.post('/signup', (req, res, next) => { 
-    // Creating empty user object 
-    let newUser = new User(); 
-    if(newUser.isPresent()){
-        console.log(newUser.isPresent());
-        return res.status(400).render("signUpPage.ejs",{ 
-            message : "Cet utilisateur existe déjà"
+// User login api
+router.post("/login", (req, res) => {
+  console.log("user login");
+  // Find user with requested email
+  User.findOne({ username: req.body.username }, function (err, user) {
+    if (user === null) {
+      return res.status(400).render("loginPage.ejs", {
+        message: "Ce compte n'existe pas",
+      });
+    } else {
+      if (user.validPassword(req.body.password)) {
+        return res.status(201).render("index.ejs", {
+          ma_variable: req.body.username,
         });
+      } else {
+        return res.status(400).render("loginPage.ejs", {
+          message: "Nom d'utilisateur, ou mot de passe incorrect",
+        });
+      }
     }
+  });
+});
 
-    // Initialize newUser object with request data 
-    newUser.username = req.body.username, 
+// User signup api
+router.post("/signup", (req, res, next) => {
+  // Creating empty user object
 
-    newUser.email = req.body.email,
+  User.findOne({ username: req.body.username }, function (err, user) {
+    if (user !== null) {
+      return res.status(400).render("signUpPage.ejs", {
+        message: "Cet utilisateur existe déjà",
+      });
+    } else {
+      let newUser = new User();
+      // Initialize newUser object with request data
+      (newUser.username = req.body.username),
+        (newUser.email = req.body.email),
+        (newUser.password = req.body.password);
 
-    newUser.password=req.body.password
+      // Call setPassword function to hash password
+      newUser.setPassword(req.body.password);
 
-    // Call setPassword function to hash password 
-    newUser.setPassword(req.body.password); 
-    console.log(JSON.stringify(newUser));
-
-    // Save newUser object to database 
-    newUser.save((err, User) => { 
-        if (err) { 
-            return res.status(400).send({ 
-                message : "Failed to add user."
-            }); 
-        } 
-        else { 
-            return res.status(201).render("signUpPage.ejs",{ 
-                message : "Inscription aves succés!"
-            });
-        } 
-    }); 
-}); 
-// Export module to allow it to be imported in other files 
+      // Save newUser object to database
+      newUser.save((err, User) => {
+        if (err) {
+          return res.status(400).send({
+            message: "Failed to add user.",
+          });
+        } else {
+          return res.status(201).render("signUpPage.ejs", {
+            message: "Inscription aves succés!",
+          });
+        }
+      });
+    }
+  });
+});
+// Export module to allow it to be imported in other files
 module.exports = router;
