3addc39cd120e2aa93e5583051c245f2c6551402
Najim Kachout <najimkachout4904@gmail.com>
2022-12-30 23:56:57 +0100

    add function of add update and delete task

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useCallback } from "react";
 import { Box, IconButton, useTheme } from "@mui/material";
 import { ColorModeContext, tokens } from "../theme";
 import ButtonDownload from "./ButtonDownload";
@@ -12,6 +12,7 @@ import {
   WeekView,
   MonthView,
   Appointments,
+  AppointmentTooltip,
   ViewSwitcher,
   Toolbar,
   DateNavigator,
@@ -25,6 +26,54 @@ export default function Calendar() {
   const colorMode = useContext(ColorModeContext);
 
   const [schedulerData, setSchedulerData] = useState([]);
+  const [addedAppointment, setAddedAppointment] = React.useState({});
+  const [
+    isAppointmentBeingCreated,
+    setIsAppointmentBeingCreated,
+  ] = React.useState(false);
+
+  const onCommitChanges = useCallback(
+    ({ added, changed, deleted }) => {
+      if (added) {
+        const startingAddedId =
+          schedulerData.length > 0
+            ? schedulerData[schedulerData.length - 1].id + 1
+            : 0;
+        console.log(added);
+        setSchedulerData([...schedulerData, { id: startingAddedId, ...added }]);
+        addEvent({ id: startingAddedId, ...added });
+      }
+      if (changed) {
+        updateEvent(changed);
+        setSchedulerData(
+          schedulerData.map((appointment) =>
+            changed[appointment.id]
+              ? { ...appointment, ...changed[appointment.id] }
+              : appointment
+          )
+        );
+      }
+      if (deleted !== undefined) {
+        setSchedulerData(
+          schedulerData.filter((appointment) => {
+            if (appointment.id !== deleted) {
+              return true;
+            } else {
+              deleteEvent(appointment);
+              return false;
+            }
+          })
+        );
+      }
+      setIsAppointmentBeingCreated(false);
+    },
+    [setSchedulerData, setIsAppointmentBeingCreated, schedulerData]
+  );
+  
+  const onAddedAppointmentChange = React.useCallback((appointment) => {
+    setAddedAppointment(appointment);
+    setIsAppointmentBeingCreated(true);
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,11 +83,72 @@ export default function Calendar() {
         headers: { "Content-Type": "application/json" },
       });
       const json = await data.json();
-      console.log(json.vevent);
-      setSchedulerData(json.vevent);
+      if (json.vevent.length > 0) {
+        setSchedulerData(json.vevent);
+      }
     };
     fetchData().catch(console.error);
   }, []);
+
+  let addEvent = (added) => {
+    const fetchData = async () => {
+      var tzoffset = (new Date()).getTimezoneOffset() * 60000;
+      delete added.allDay;
+      added.startDate = new Date(added.startDate - tzoffset).toISOString(true);
+      added.endDate = new Date(added.endDate - tzoffset).toISOString(true);
+      const data = await fetch(
+        "http://127.0.0.1:8080/calendar/addEvent",
+        {
+          method: "POST",
+          body: JSON.stringify({ event: added }),
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      const json = await data.json();
+      console.log(json);
+    };
+    fetchData().catch(console.error);
+  };
+
+  let deleteEvent = (deleted) => {
+    const fetchData = async () => {
+      delete deleted.allDay;
+      console.log(deleted);
+      const data = await fetch(
+        "http://127.0.0.1:8080/calendar/deleteEvent",
+        {
+          method: "DELETE",
+          body: JSON.stringify({event: deleted}),
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      const json = await data.json();
+      console.log(json.response);
+    };
+    fetchData().catch(console.error);
+  };
+
+  let updateEvent = (updated) => {
+    const fetchData = async () => {
+      let updatedTemp = JSON.parse(JSON.stringify(updated))
+      if(updatedTemp[Object.keys(updatedTemp)[0]].hasOwnProperty('notes')){
+        delete Object.assign(updatedTemp[Object.keys(updatedTemp)[0]], {description: updatedTemp[Object.keys(updatedTemp)[0]].notes})['notes'];
+      }
+      if(updatedTemp[Object.keys(updatedTemp)[0]].hasOwnProperty('title')){
+        delete Object.assign(updatedTemp[Object.keys(updatedTemp)[0]], {summary: updatedTemp[Object.keys(updatedTemp)[0]].title})['title'];
+      }
+      console.log(updatedTemp)
+      const data = await fetch(
+        "http://127.0.0.1:8080/calendar/updateEvent",
+        {
+          method: "POST",
+          body: JSON.stringify({event: updatedTemp}),
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    };
+    fetchData().catch(console.error);
+  };
 
   return (
     <div id="calendar">
@@ -58,12 +168,17 @@ export default function Calendar() {
         <ButtonDownload />
         <Scheduler locale={"fr-FR"} data={schedulerData} firstDayOfWeek={1}>
           <ViewState />
-          <EditingState />
+          <EditingState
+            onCommitChanges={onCommitChanges}
+            addedAppointment={addedAppointment}
+            onAddedAppointmentChange={onAddedAppointmentChange}
+          />
           <IntegratedEditing />
           <WeekView startDayHour={7} endDayHour={22} displayName="Semaine" />
           <MonthView displayName="Mois" />
-          <Appointments/>
+          <Appointments />
           <Toolbar />
+          <AppointmentTooltip showOpenButton showDeleteButton />
           <AppointmentFormCustom />
           <DateNavigator />
           <ViewSwitcher />
